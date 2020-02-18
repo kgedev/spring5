b956c13ac112d5ed3860754bf21ffe0e75e0ec1e
g.kuznetsov <g.kuznetsov@siams.com>
2020-02-18 09:26:51 +0500

    s5: add DI

--- src/main/java/ru/kgedev/app/TestSpring.java
+++ src/main/java/ru/kgedev/app/TestSpring.java
@@ -1,16 +1,14 @@
 package ru.kgedev.app;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.kgedev.app.api.Music;
 
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        Music music = context.getBean("jazzMusic", Music.class);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic();
 
